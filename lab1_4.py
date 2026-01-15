ee98cc2916ea1d02dbac5e80d7157ff6c0093584
SofiaSoto27 <ssotoazc@calpoly.edu>
2026-01-15 00:37:56 +0000

    lab 1.4 complete

--- lab1_4.py
+++ lab1_4.py
@@ -1,1 +1,17 @@
 pass
+num1 = 16.8
+num2 = 9
+num3 = 7.2 
+def calculate_average(num1, num2, num3):
+    return (float(num1) + num2 + float(num3)) / 3
+print(calculate_average(num1, num2, num3))
+
+number = 93.96
+def add_tax(number):
+    return number * 1.10
+print(add_tax(number))
+
+name = "Sofia"
+def greet_user(name):
+    return "Hello " + name
+print(greet_user(name))
